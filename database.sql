1bad7f01cab8ba9246ab706da34d30d8116f3757
nao580 <nao580@gmail.com>
2014-10-19 01:06:58 +0200

    dodanie  przykladowych insertow do bazy

--- database.sql
+++ database.sql
@@ -10,11 +10,11 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
 -- -----------------------------------------------------
--- Table `user`
+-- Table 'user`
 -- -----------------------------------------------------
 
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS 'user` (
   `id_user` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `login` VARCHAR(200) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
@@ -34,35 +34,55 @@ CREATE TABLE IF NOT EXISTS `photo` (
   `title` VARCHAR(45) NULL,
   `description` VARCHAR(200) NULL,
   `path` VARCHAR(2048) NOT NULL,
+  `filename` VARCHAR(45) NOT NULL,
   `tags` VARCHAR(200) NULL,
   `user_id_user` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id_photo`, `user_id_user`),
   INDEX `fk_photo_user_idx` (`user_id_user` ASC),
   CONSTRAINT `fk_photo_user`
     FOREIGN KEY (`user_id_user`)
-    REFERENCES `mydb`.`user` (`id_user`)
+    REFERENCES 'user` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARACTER SET = latin1;
 
 
 
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_4819.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_4835.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_4919.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_4956.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_5005.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_5012.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_5195.jpg`, NULL, 1);
+
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', '_MG_4819.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', '_MG_4835.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', '_MG_4919.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', '_MG_4956.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', '_MG_5005.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', '_MG_5012.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', '_MG_5195.jpg', NULL, 1);
 
 
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\BackgroundCircle.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\BlogPortfolio_Logo.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\Death_Expectation.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\Dratewka_logo_Final.png`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\Dratewka_website.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_5012.jpg`, NULL, 1);
-INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, `upload\9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18\_MG_5195.jpg`, NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'BackgroundCircle.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'BlogPortfolio_Logo.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Death_Expectation.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Dratewka_logo_Final.png', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Dratewka_website.jpg', NULL, 1);
+
+
+
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Everything_IS_conected.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Love_Fences.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'My_template_V3.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'My_template_V4.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'NANO_2014.jpg', NULL, 1);
+
+
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'NaoDesign_Wallpeper.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'NaoDesign_Wallpeper_2.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'NaoDesign_Wallpeper_3.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Nario_Time_to_adventure.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Panel09.jpg', NULL, 1);
+
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Portfolio_Blog.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'Show_Your_Darkness2.jpg', NULL, 1);
+INSERT INTO `photo` (`id_photo`, `title`, `description`, `path`, `filename`, `tags`, `user_id_user`) VALUES (NULL, NULL, NULL, 'upload/9Fv2a6h392t8ST4L46850151C156PeR7_2014-10-18/', 'WSNHiD_Logo.jpg', NULL, 1);
+
 
 -- -----------------------------------------------------
 -- Drop table
@@ -70,5 +90,5 @@ INSERT INTO `mydb`.`photo` (`id_photo`, `title`, `description`, `path`, `tags`, 
 
 
 
-DROP TABLE IF EXISTS `user` ;
+DROP TABLE IF EXISTS 'user` ;
 DROP TABLE IF EXISTS `photo` ;
