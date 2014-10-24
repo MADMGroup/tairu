f8e0d362f672d833b90d5bc01832fa5ff4b7aba8
nao580 <nao580@gmail.com>
2014-10-24 20:30:59 +0200

    added delete & uplaod function
    -nie działa przekierowanie w IE
    -max_file_size post (8MB hostinger.pl) problem

--- userpanel.php
+++ userpanel.php
@@ -11,54 +11,46 @@ session_start();
 <title>Tairu</title>
 <link rel="stylesheet" href="css/reset.css">
 <link rel="stylesheet" href="css/userpanel.css">
+
+
 </head>
 <body>
 <?php
-   if(empty($_COOKIE['islogged']))
+if(empty($_COOKIE['islogged']))
    {
 	   			 ?>
-             
-              <div class="start">
-
-    <div class="blockOuter">
-
-
-	  
-		<div style="display:table;border-spacing:20px;margin:0 auto;">
-	  <div id="loading">	  </div>
-
-	  <div class="statement">Czas sesji wygasł. Trwa przekierowanie...</div>
-
-
-
-	  </div>
-
-	  </div>
-
-	  </div>
-
-          <?php header( "Refresh:2; url=login.php", true, 303);
+<div class="start">
+  <div class="blockOuter">
+    <div style="display:table;border-spacing:20px;margin:0 auto;">
+      <div id="loading"> </div>
+      <div class="statement">Czas sesji wygasł. Trwa przekierowanie...</div>
+    </div>
+  </div>
+</div>
+<?php header( "Refresh:2; url=login.php", true, 303);
    }
+   else
+   {
 
    if(isset($_SESSION['nick']) && isset($_SESSION['ip']))
    {
 ?>
-    <div class="content">
-  <div class="fake">
-    <div id="pa">
-      <div id="logopa"><img src="css/images/logo.png"  width="150" height="167"/></div>
-      <div class="line"></div>
-      <div class="line_2"></div>
-      <div id="menu"> <a href="logout.php" class="icons">Logout</a> </div>
-    </div>
+<div class="content">
+<div class="fake">
+  <div id="pa">
+    <div id="logopa"><img src="css/images/logo.png"  width="150" height="167"/></div>
+    <div class="line"></div>
+    <div class="line_2"></div>
+    <div id="menu"> <a href="logout.php" class="icons">Logout</a> </div>
   </div>
-  <div class="line"></div>
-  <div class="line_2"></div>
-  
-    	<?php
+</div>
+<div class="line"></div>
+<div class="line_2"></div>
+<form name="galery_list" action="services.php" method="POST">
+  <?php
 		
 		 include_once('config.php');
-		 $mysqli = new mysqli($db['host'], $db['user'], $db['password'], $db['database']);
+		 $mysqli = @new mysqli($db['host'], $db['user'], $db['password'], $db['database']);
 		 
 		  if($wynik = $mysqli -> query("SELECT id_photo, path, filename FROM `photo`"))
 		  {
@@ -67,11 +59,11 @@ session_start();
 			  while( $photo = $wynik->fetch_assoc())
 	{	
 			?>
-			<div class="image_block">
-   <div class="image_block_inner">
-   		<div class="checkbox">
-        <?php
-		echo '<input type="checkbox" id="'. $photo["id_photo"].'" /> <label for="'. $photo["id_photo"].'"></label></div>';
+  <div class="image_block">
+  <div class="image_block_inner">
+  <div class="checkbox">
+  <?php
+		echo '<input type="checkbox" id="'. $photo["id_photo"].'" name="checkedItem[]" value="'. $photo["id_photo"].'" /> <label for="'. $photo["id_photo"].'"></label></div>';
         echo ' <span class="helper"></span><img class="image" src="' . $photo["path"] .'thumb_'. $photo["filename"] .'"/> </div></div>';
 
 	}
@@ -83,48 +75,53 @@ session_start();
 /* Zamykamy połączenie z bazą */
 $mysqli->close();
 			  
-		  ?>
+		  ?><input type="submit" style="display:none;" name="del" value="Delete" />
+          
+          </form>
+   <form>
+
+  <svg class="buttons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" onclick="document.galery_list.del.click()"  width='172' height='40'>
   
+  <rect fill='black' x='0' y='0' width='100%' height='100%'/> <text x='50%' y='50%' dy='.3em' font-size='14'  font-family='Segoe UI' text-anchor='middle' fill='white' > Delete </text>
   
+  </svg>
+</form>
+
+<form name="upload_form" action="services.php" method="post" enctype="multipart/form-data">
+  <svg class="buttons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" onclick="document.getElementById('upl').click();"  width='172' height='40'>
   
+  <rect fill='black' x='0' y='0' width='100%' height='100%'/> <text x='50%' y='50%' dy='.3em' font-size='14'  font-family='Segoe UI' text-anchor='middle' fill='white' > Upload </text>
+  
+  </svg>
+       <input name="files[]" type="file" multiple name="upl" id="upl" value="Upload"  style="display:none;" onChange="this.form.submit()" />
+</form>
 
 
 
-
-
+  </form>
 <?php
 
    }
    else
    {
 	      			 ?>
-             
-              <div class="start">
-
-    <div class="blockOuter">
-
-
-	  
-		<div style="display:table;border-spacing:20px;margin:0 auto;">
-	  <div id="loading">	  </div>
-
-	  <div class="statement">Nie jesteś zalogowany. Trwa przekierowanie...</div>
-
-
-
-	  </div>
-
-	  </div>
-
-	  </div>
-
-          <?php header( "Refresh:2; url=login.php", true, 303);
+<div class="start">
+  <div class="blockOuter">
+    <div style="display:table;border-spacing:20px;margin:0 auto;">
+      <div id="loading"> </div>
+      <div class="statement">Nie jesteś zalogowany. Trwa przekierowanie...</div>
+    </div>
+  </div>
+</div>
+<?php header( "Refresh:2; url=login.php", true, 303);
+   }
    }
 
 ?>
-
 </body>
-</html>
-<?php
+
+
+
+</html><?php
 ob_end_flush();
 ?>
